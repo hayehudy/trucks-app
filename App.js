d12a21c6dee4ae7561149e244013cb6779c5b560
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-02-15 16:34:42 +0200

    err

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import LogInPage from "./pages/LoginPage/Login";
 import StartPage from "./pages/StartPage/Start";
 import DetailsPage from "./pages/DetailsPage/Details";
